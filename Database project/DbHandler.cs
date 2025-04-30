b4a7a52c2f4e6ec0448b370683dd53ad68c9fdbe
Abdelrahman <boudysallam1@gmail.com>
2025-04-30 23:49:48 +0300

    insertguest

--- Database project/DbHandler.cs
+++ Database project/DbHandler.cs
@@ -64,31 +64,45 @@ namespace Database_project
             }
         }
 
-        public int InsertGuest(string firstName, string lastName, string email, string phoneNumber, string streetName, string flatNo, string city, string gFloor)
+        public int InsertGuest(int guestId, string firstName, string lastName, string email, string phoneNumber, string streetName, string flatNo, string city, string gFloor)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
 
-                // Insert query
-                string query = @"
-                INSERT INTO Guest (First_Name, Last_Name, Email, Phone_Number, Street_Name, Flat_No, City, GFloor)
-                VALUES (@First_Name, @Last_Name, @Email, @Phone_Number, @Street_Name, @Flat_No, @City, @GFloor);
-                SELECT SCOPE_IDENTITY();";  // This will return the auto-incremented GuestID
+                // Check if guest with given ID exists
+                string checkQuery = "SELECT COUNT(*) FROM Guest WHERE GuestID = @GuestID";
+                SqlCommand checkCmd = new SqlCommand(checkQuery, connection);
+                checkCmd.Parameters.AddWithValue("@GuestID", guestId);
 
-                SqlCommand cmd = new SqlCommand(query, connection);
-                cmd.Parameters.AddWithValue("@First_Name", firstName);
-                cmd.Parameters.AddWithValue("@Last_Name", lastName);
-                cmd.Parameters.AddWithValue("@Email", email);
-                cmd.Parameters.AddWithValue("@Phone_Number", phoneNumber);
-                cmd.Parameters.AddWithValue("@Street_Name", streetName);
-                cmd.Parameters.AddWithValue("@Flat_No", flatNo);
-                cmd.Parameters.AddWithValue("@City", city);
-                cmd.Parameters.AddWithValue("@GFloor", gFloor);
+                int count = (int)checkCmd.ExecuteScalar();
+                if (count > 0)
+                {
+                    // Guest already exists
+                    return 0;
+                }
+                else
+                {
 
-                // Execute the query and retrieve the GuestID
-                int guestId = Convert.ToInt32(cmd.ExecuteScalar());  // This will give the last inserted GuestID
-                return guestId;
+                    // Insert new guest
+                    string insertQuery = @"
+        INSERT INTO Guest (First_Name, Last_Name, Email, Phone_Number, Street_Name, Flat_No, City, GFloor)
+        VALUES (@First_Name, @Last_Name, @Email, @Phone_Number, @Street_Name, @Flat_No, @City, @GFloor);
+        SELECT SCOPE_IDENTITY();";
+
+                    SqlCommand insertCmd = new SqlCommand(insertQuery, connection);
+                    insertCmd.Parameters.AddWithValue("@First_Name", firstName);
+                    insertCmd.Parameters.AddWithValue("@Last_Name", lastName);
+                    insertCmd.Parameters.AddWithValue("@Email", email);
+                    insertCmd.Parameters.AddWithValue("@Phone_Number", phoneNumber);
+                    insertCmd.Parameters.AddWithValue("@Street_Name", streetName);
+                    insertCmd.Parameters.AddWithValue("@Flat_No", flatNo);
+                    insertCmd.Parameters.AddWithValue("@City", city);
+                    insertCmd.Parameters.AddWithValue("@GFloor", gFloor);
+
+                    int newGuestId = Convert.ToInt32(insertCmd.ExecuteScalar());
+                    return newGuestId;
+                }
             }
         }
 
