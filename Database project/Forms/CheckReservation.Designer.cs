4f08a27366257e2cf81218cca1fbe4eea9f0796e
Abdelrahman <boudysallam1@gmail.com>
2025-05-02 00:16:38 +0300

    view reservation

--- Database project/Forms/CheckReservation.Designer.cs
+++ Database project/Forms/CheckReservation.Designer.cs
@@ -38,14 +38,15 @@
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.back_btn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(354, 224);
+            this.label1.Location = new System.Drawing.Point(354, 246);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 16);
             this.label1.TabIndex = 0;
@@ -53,7 +54,7 @@
             // 
             // roomNumber
             // 
-            this.roomNumber.Location = new System.Drawing.Point(341, 243);
+            this.roomNumber.Location = new System.Drawing.Point(341, 265);
             this.roomNumber.Name = "roomNumber";
             this.roomNumber.Size = new System.Drawing.Size(120, 22);
             this.roomNumber.TabIndex = 1;
@@ -62,7 +63,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(363, 116);
+            this.label2.Location = new System.Drawing.Point(363, 138);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 20);
             this.label2.TabIndex = 2;
@@ -70,7 +71,7 @@
             // 
             // GuestID
             // 
-            this.GuestID.Location = new System.Drawing.Point(341, 139);
+            this.GuestID.Location = new System.Drawing.Point(341, 161);
             this.GuestID.MaxLength = 100000;
             this.GuestID.Name = "GuestID";
             this.GuestID.Size = new System.Drawing.Size(120, 22);
@@ -80,7 +81,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(39, 28);
+            this.label3.Location = new System.Drawing.Point(39, 50);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(263, 32);
             this.label3.TabIndex = 4;
@@ -95,7 +96,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(122, 83);
+            this.label4.Location = new System.Drawing.Point(122, 105);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 29);
             this.label4.TabIndex = 5;
@@ -105,7 +106,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(122, 191);
+            this.label5.Location = new System.Drawing.Point(122, 213);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(114, 29);
             this.label5.TabIndex = 6;
@@ -113,7 +114,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(660, 180);
+            this.button1.Location = new System.Drawing.Point(660, 202);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 7;
@@ -130,7 +131,33 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(859, 236);
             this.dataGridView1.TabIndex = 8;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(650, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Back
+            // 
+            this.Back.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Back.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Back.FlatAppearance.BorderSize = 3;
+            this.Back.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Back.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Back.Location = new System.Drawing.Point(12, 11);
+            this.Back.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(70, 37);
+            this.Back.TabIndex = 70;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
             // 
             // back_btn
             // 
@@ -146,8 +173,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.ClientSize = new System.Drawing.Size(883, 550);
-            this.Controls.Add(this.back_btn);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
@@ -177,6 +206,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         public System.Windows.Forms.TextBox roomNumber;
-        private System.Windows.Forms.Button back_btn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Back;
     }
 }
